94eef00e5564373ba27d024ad4ceb175340faabc
ryota-sb <ryota.sb.1206@gmail.com>
2022-07-14 21:59:08 +0900

    コンソール入力テスト

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+// 関数定義
+// function message(count: number): string {
+//   return "Hey".repeat(count);
+// }
+// console.log(message(10));
+
+// 引数の型によって出力を変える
+// function message(value: number | string) {
+//   if (typeof value === "number") {
+//     console.log(value * 2);
+//   } else {
+//     console.log(value.repeat(3));
+//   }
+// }
+
+// console.log(message("fack"));
+
+// function numDouble(value: number): number {
+//   return value * 2;
+// }
+
+// console.log(numDouble(1_000_000));
+
+// import { createInterface } from "readline";
+
+// const rl = createInterface({ input: process.stdin, output: process.stdout });
+
+// rl.question("文字を入力してください:", (line) => {
+//   console.log(`${line}が入力されました`);
+//   rl.close;
+// });
